c46df58e18cfbe6a3ce838f4a803438f2dfb7370
Carlos Araújo <111444349+carlosarauj@users.noreply.github.com>
2024-04-15 22:01:17 -0300

    Add files via upload

---
+++ py.py
@@ -0,0 +1,11 @@
+soma = 1 + 1
+multip = 2*2
+div = 4/2
+pot = 2**6
+
+print("soma", soma)
+print("multip", multip)
+print("div", div)
+print("pot", pot)
+
+#usa o terminal com: python nomedoarquivo.py
